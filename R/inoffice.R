bb19b0df5f392ec628f0120753cc2f0556aa6bb7
bioguo <guokai8@gmail.com>
2019-05-09 13:58:51 -0500

    add read table from pptx and docx

---
+++ R/inoffice.R
@@ -0,0 +1,102 @@
+##' read tables from ppt and word
+##' @importFrom officer read_docx
+##' @importFrom officer docx_summary
+##' @importFrom officer pptx_summary
+##' @importFrom magrittr %>%
+##' @importFrom dplyr select_
+##' @importFrom dplyr filter_
+##' @param filename input filename
+##' @param format pptx or docx
+##' @param header use first row as column name
+##' @export
+##' @author Kai Guo
+inoffice <- function(filename, format = NULL, header = TRUE){
+    if(is.null(format)){
+        format = .getext(filename)
+    }
+    format = tolower(format)
+    if (format == "ppt" | format == "pptx") {
+        format = "ppt"
+    }
+    if (format == "doc" | format == "docx") {
+        format = "doc"
+    }
+    if(format == "ppt"){
+        doc <- read_pptx(filename)
+        content <- pptx_summary(doc)
+        tent <- tapply(content$id,
+               content$content_type,
+               function(x) length(unique(x)))
+        tent["slides"] <- length(unique(content$slide_id))
+    }
+    if(format == "doc"){
+        doc <- read_docx(filename)
+        content <- docx_summary(doc)
+        tent <- tapply(content$doc_index,
+                       content$content_type,
+                       function(x) length(unique(x)))
+    }
+    cat("Here are the details in the ",filename,":\n")
+    print(tent)
+    table_cells <- subset(content, content_type %in% "table cell")
+    if(format == "ppt"){
+        len <- length(unique(table_cells$slide_id))
+        res<-vector(len,mode="list")
+        for(i in 1:len){
+            sub_table <- filter_(table_cells, ~slide_id == i)
+            for(j in unique(sub_table$id)){
+                data <- subset(sub_table, id == j, c(row_id, cell_id, text) )
+                data <- tapply(data$text,
+                        list(row_id = data$row_id,
+                        cell_id = data$cell_id
+                   ), FUN = I )
+                res[[i]][[j]] <- data
+            }
+        }
+        names(res) <- paste("slide",1:len,sep="")
+        res <- unlist(res, recursive = FALSE)
+        }
+    if(format == "doc"){
+        len <- length(unique(table_cells$doc_index))
+        res<-vector(len,mode="list")
+        for(i in unique(table_cells$doc_index)){
+            data <- subset(table_cells, doc_index == i, c(row_id, cell_id, text) )
+            data <- tapply(data$text,
+                           list(row_id = data$row_id,
+                                cell_id = data$cell_id
+                           ), FUN = I )
+            res[[i]] <- data
+        }
+        res <- Filter(Negate(is.null), res)
+        names(res) <- paste("Table",1:length(res),sep="")
+    }
+    res <- lapply(res, function(x) as.data.frame(x))
+    if(isTRUE(header)){
+        res <- lapply(res, function(x).setcolnames(x))
+    }
+    return(res)
+    }
+
+##' read table from pptx
+##' @name inpptx
+##' @param filename input filename
+##' @param header use first row as column name
+##' @author Kai Guo
+##' @export
+inpptx <- function(filename, header = FALSE){
+    res <- inoffice(filename, format = "pptx", header = header)
+    return(res)
+}
+
+##' read table from docx
+##' @name indocx
+##' @param filename input filename
+##' @param header use first row as column name
+##' @author Kai Guo
+##' @export
+indocx <- function(filename, header = FALSE){
+    res <- inoffice(filename, format = "docx", header = header)
+    return(res)
+}
+
+
--- R/misc.R
+++ R/misc.R
@@ -4,3 +4,15 @@
     pos <- regexpr("\\.([[:alnum:]]+)$", x)
     ifelse(pos > -1L, substring(x, pos + 1L), "")
 }
+
+##' set first row as column name
+##' @importFrom utils type.convert
+##' @param file input data frame
+##' @author Kai Guo
+##' @export
+.setcolnames <- function(file){
+    names(file) <- as.matrix(file[1, ])
+    file <- file[-1, ]
+    file[] <- lapply(file, function(x) type.convert(as.character(x)))
+    file
+}
