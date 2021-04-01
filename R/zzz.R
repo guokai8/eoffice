63ea3d8ef72c6f98216110b2deacbb37a7c34ebe
bioguo <guokai8@gmail.com>
2021-03-31 19:22:11 -0500

    Add files via upload

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,3 @@
 .onLoad <- function(libname, pkgname) {
-    old <- options()         
     options(stringsAsFactors = FALSE)
-    on.exit(options(old)) 
-}
-if(getRversion() >= "2.15.1") {
-    utils::globalVariables(c(".","Abundance", "Group","LDA","LDAscore", "OTU",
-                             "Phylum","Sample","direction",
-                             "log2FoldChange","ma","p.adj",
-                             "p.value", "tax","type", "val"))
 }
